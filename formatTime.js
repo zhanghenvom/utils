3f479ef621ae1361f4061762523678aed90f6188
zhanghenvom <1640867963@qq.com>
2020-09-17 16:47:00 +0800

    2020.9.17

---
+++ formatTime.js
@@ -0,0 +1,22 @@
+/** 
+ * 时间戳转化为年 月 日 时 分 秒 
+ * number: 传入时间戳 
+ * format：返回格式，支持自定义，但参数必须与formateArr里保持一致,默认格式'Y年M月D日h点m分s秒'
+ */
+function formatTimeTwo(number, format = 'Y年M月D日h点m分s秒') {
+    var formateArr = ['Y', 'M', 'D', 'h', 'm', 's'];
+    var returnArr = [];
+    var date = new Date(number);
+    returnArr.push(
+        date.getFullYear(),
+        formatNumber(date.getMonth() + 1),
+        formatNumber(date.getDate()),
+        formatNumber(date.getHours()),
+        formatNumber(date.getMinutes()),
+        formatNumber(date.getSeconds())
+    )
+    for (var i in returnArr) {
+        format = format.replace(formateArr[i], returnArr[i]);
+    }
+    return format;
+}

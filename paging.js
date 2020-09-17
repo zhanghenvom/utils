b6d1272ecb6c910aac1843227d93ed2fec15aace
zhanghenvom <1640867963@qq.com>
2020-09-17 16:34:39 +0800

    2020.9.17

---
+++ paging.js
@@ -0,0 +1,12 @@
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+/**
+ * @description: 分页函数
+ * @param 数组/每页条数/当前页数
+ * @return Array,空=>无数据
+ */
+let methdos = (array, page, number) => {
+    return array.filter((e, i, a) => page * (number - 1) <= i && i < page * number)
+}
+
+console.log(methdos([...array], 5, 6));   
